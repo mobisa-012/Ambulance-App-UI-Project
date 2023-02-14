cc6a5afdc65a5aee3cf21f2a3af3e0fcffb2a067
mobisa-012 <mobisakwamboka@gmail.com>
2023-02-14 05:29:45 +0300

    map_screen ongoing edit

--- ambulance_app_ui/lib/screens/map_screen/map_screen.dart
+++ ambulance_app_ui/lib/screens/map_screen/map_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:ambulance_app_ui/core/const/text_constants.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
@@ -13,9 +15,22 @@ class MapScreen extends StatefulWidget {
 
 class _MapScreenState extends State<MapScreen> {
   final mapController = MapController();
+  Timer? timer;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        elevation: 0,
+        backgroundColor: Colors.deepPurpleAccent,
+        // shape: ContinuousRectangleBorder(
+        //   borderRadius: BorderRadius.circular(20),
+        // ),
+        child: Icon(
+          Icons.navigation_rounded,
+          color: Colors.white,
+        ),
+      ),
       appBar: AppBar(
         elevation: 0,
         scrolledUnderElevation: 1.0,
@@ -28,9 +43,42 @@ class _MapScreenState extends State<MapScreen> {
           FlutterMap(
             mapController: mapController,
             options: MapOptions(
-              center: LatLng(0.0,0.0)
-            ),
-            children: [],
+                onPositionChanged: (MapPosition position, bool hasGesture) {
+                  timer?.cancel();
+                  timer = Timer(Duration(seconds: 1), () {
+                    print(position);
+                  });
+                },
+                // onTap: () {},
+                center: LatLng(45.37, 0.1785),
+                zoom: 18,
+                maxZoom: 15,
+                minZoom: 0,
+                bounds: LatLngBounds(
+                  LatLng(-1.28333, 36.81667),
+                  LatLng(51.25709, 0.34018),
+                ),
+                maxBounds: LatLngBounds(LatLng(-90, -180), LatLng(90, 180))),
+            children: [
+              TileLayer(
+                urlTemplate: "https://tile.openstreetmap.org/{z}/{x}/{y}.png",
+                userAgentPackageName: 'dev.fleaflet.flutter_map.example',
+                retinaMode: MediaQuery.of(context).devicePixelRatio > 1.0,
+                subdomains: ['a','b','c'],
+              ),
+              MarkerLayer(
+                markers: [
+                  Marker(
+                    height: 80,
+                    width: 80,
+                    point: mapController.center, 
+                    builder: (context) => Container(
+                      child: Icon(Icons.location_on),
+                    ),
+                    )
+                ],
+              )
+            ],
           )
         ],
       ),
