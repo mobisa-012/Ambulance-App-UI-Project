ce490bc3c27bfb4d4435c2c87f97f38251a5de28
mobisa-012 <mobisakwamboka@gmail.com>
2023-02-22 10:07:40 +0300

    chnaged onboarding sccreen UI

--- ambulance_app_ui/lib/screens/onboarding/onboarding_screen.dart
+++ ambulance_app_ui/lib/screens/onboarding/onboarding_screen.dart
@@ -1,9 +1,7 @@
 import 'package:ambulance_app_ui/core/const/colors.dart';
-import 'package:ambulance_app_ui/core/const/data_constants.dart';
-import 'package:ambulance_app_ui/core/const/text_constants.dart';
-import 'package:ambulance_app_ui/screens/verification_page/verification_page.dart';
+import 'package:ambulance_app_ui/core/const/path_constants.dart';
+import 'package:ambulance_app_ui/screens/login/phone_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:page_view_dot_indicator/page_view_dot_indicator.dart';
 
 class OnboardingPage extends StatefulWidget {
   const OnboardingPage({super.key});
@@ -13,88 +11,114 @@ class OnboardingPage extends StatefulWidget {
 }
 
 class _OnboardingPageState extends State<OnboardingPage> {
-  int selectedPage = 0;
-  late PageController pageController;
-
-  @override
-  void initState() {
-    selectedPage = 0;
-    pageController = PageController(initialPage: selectedPage);
-    super.initState();
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: AppColors.homeBackground,
-      body: SafeArea(
-          child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Expanded(
-            child: _createPageView(pageController, context),
-            flex: 2,
-          ),
-          Expanded(
-            child: _createStatic(context),
-          ),
-          _createButton(context),
-        ],
-      )),
+      backgroundColor: Colors.white,
+      body: Container(
+        padding:
+            const EdgeInsets.only(top: 150, left: 12, right: 12, bottom: 10),
+        decoration: const BoxDecoration(color: AppColors.homeBackground),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            _createChurchLogo(),
+            const SizedBox(
+              height: 60,
+            ),
+            _createName(),
+            const SizedBox(
+              height: 90,
+            ),
+            _createJoinFamilyButton(context),
+            const SizedBox(
+              height: 120,
+            ),
+            _createDevsName(),
+          ],
+        ),
+      ),
     );
   }
 
-  Widget _createButton(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        ElevatedButton(
-          style: ElevatedButton.styleFrom(
-            backgroundColor: Colors.blueAccent,
-            elevation: 0,
-            padding: EdgeInsets.all(10),
-            textStyle: TextStyle(
-              fontSize: 18,
-              color: Colors.white,
-            ),
-          ),
-          onPressed: () {
-            Navigator.of(context).pushReplacement(
-                MaterialPageRoute(builder: (_) => VerificationPage()));
-          },
-          child: Text(
-            TextConstants.orderAmbulance,
-          ),
+  Widget _createChurchLogo() {
+    return Center(
+      child: Image.asset(PathConstants.onboarding),
+    );
+  }
+
+  Widget _createName() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: const [
+        Text(
+          'Hosy',
+          style: TextStyle(
+              fontSize: 25,
+              fontWeight: FontWeight.w800,
+              fontFamily: 'Roboto',
+              color: AppColors.homeBackground),
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        Text(
+          'Hosy',
+          style: TextStyle(
+              fontSize: 25,
+              fontWeight: FontWeight.bold,
+              fontFamily: 'Roboto',
+              color: AppColors.homeBackground),
         ),
       ],
     );
   }
 
-  Widget _createPageView(PageController pageController, BuildContext context) {
-    return PageView(
-      controller: pageController,
-      children: DataConstants.onboardingTiles,
-      scrollDirection: Axis.horizontal,
-      onPageChanged: (page) {
-        setState(() {
-          selectedPage = page;
-        });
-      },
+  Widget _createJoinFamilyButton(BuildContext context) {
+    return Container(
+      height: 70,
+      width: 200,
+      decoration: BoxDecoration(
+        color: AppColors.homeBackground,
+        borderRadius: BorderRadius.circular(12),
+      ),
+      child: Center(
+        child: GestureDetector(
+          onTap: () {
+            Navigator.of(context)
+                .push(MaterialPageRoute(builder: (_) => const PhoneAuthPage()));
+          },
+          child: const Text(
+            'Order Ambulance',
+            style: TextStyle(
+                color: AppColors.textColor,
+                fontSize: 28,
+                fontFamily: 'Roboto',
+                fontWeight: FontWeight.w600),
+          ),
+        ),
+      ),
     );
   }
 
-  Widget _createStatic(BuildContext context) {
-    final pageCount = 3;
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 20),
-      child: PageViewDotIndicator(
-        currentItem: selectedPage,
-        count: pageCount,
-        selectedColor: AppColors.onboardingColor,
-        unselectedColor: Color.fromARGB(151, 11, 7, 17),
-        duration: Duration(milliseconds: 200),
-        boxShape: BoxShape.circle,
-      ),
+  Widget _createDevsName() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: const [
+        Icon(
+          Icons.copyright_outlined,
+          size: 20,
+          color: Colors.white,
+        ),
+        SizedBox(
+          width: 5,
+        ),
+        Text(
+          'Yvonne M',
+          style: TextStyle(
+              fontSize: 15, fontWeight: FontWeight.w500, color: Colors.white),
+        )
+      ],
     );
   }
 }
